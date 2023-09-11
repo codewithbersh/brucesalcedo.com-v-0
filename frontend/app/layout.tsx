36ec0ae09398e85a95f0cf64efc67ddfdcff65bd
codewithbersh <121822380+codewithbersh@users.noreply.github.com>
2023-09-11 15:16:58 +0300

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -55,13 +55,13 @@ export default function RootLayout({
 
         <Script type="text/javascript">
           {`
-    window._mfq = window._mfq || [];
+    window._mfq = window._mfq || []
   (function() {
     var mf = document.createElement("script");
     mf.type = "text/javascript"; mf.defer = true;
     mf.src = "//cdn.mouseflow.com/projects/0c7cceb2-431f-4179-8d03-f76348714add.js";
     document.getElementsByTagName("head")[0].appendChild(mf);
-  })();
+  })()
     `}
         </Script>
       </body>
